6f509ccde1701eb001457f624f55bc4675bd649a
JanEveraertEHB <72449780+JanEveraertEHB@users.noreply.github.com>
2020-11-26 09:49:13 +0100

    Feat(conversion): add conversion method

---
+++ api/src/utils/conversionHelpers.js
@@ -0,0 +1,22 @@
+
+
+
+
+const { v1: uuidv1 } = require('uuid');
+
+const ConversionHelper = {
+  /**
+   * generates UUID
+   * 
+   * @params uuid: String 
+   * @returns uuid: String
+   */
+  generateUUID: () => {
+    const uuid = uuidv1();
+    return uuid;
+  }
+
+
+}
+
+module.exports = ConversionHelper
